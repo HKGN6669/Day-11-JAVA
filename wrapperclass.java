2d419afb278097a0d52814662f61436249777334
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2024-12-12 14:33:56 +0530

    Add files via upload

---
+++ wrapperclass.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class wrapperclass{
+    public static void main(String[] args)
+    {
+        Integer a=new Integer (90);
+        System.out.println(a);
+        Double s=new Double (89);
+        System.out.println(s);
+    }
+}
